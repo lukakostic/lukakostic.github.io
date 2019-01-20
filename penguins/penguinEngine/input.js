d04bba31ff35f5e3d59c9ac0e787d82cb21d9811
Luka Kostic <3luka1@gmail.com>
2019-01-20 13:48:19 +0100

    Update input.js

--- penguins/penguinEngine/input.js
+++ penguins/penguinEngine/input.js
@@ -1,70 +1,65 @@
 "use strict";
 
-
+function __key(_name){
+	this.name = _name;
+	this.pressed = false;
+	this.down = false;
+	this.up = false;
+}
 
 function InputEngine(doc){
 
-	
-	function __key(_name){
-		this.name = _name;
-		this.pressed = false;
-		this.down = false;
-		this.up = false;
-	}
-
-	let mouse = new THREE.Vector2();
-	let keys = [];
+	this.mouse = new THREE.Vector2(0,0);
+	this.keys = [];
 		
 	for(let i = 0; i < 223; i++){
-		keys.push(new __key(i));
+		this.keys.push(new __key(i));
 	}
-		
 	
+	doc.addEventListener( 'mousemove', this.__mouseMove, false );
+	doc.addEventListener( 'mousedown', this.__mouseDown, false );
+	doc.addEventListener( 'mouseup', this.__mouseUp, false );
 	
-	let IsKeyPressed = function (k){
+}
+	
+	InputEngine.prototype.IsKeyPressed = function (k){
 		return this.keys[__getKey(k)].pressed;
 	};
 	
-	let IsKeyUp = function (k){
+	InputEngine.prototype.IsKeyUp = function (k){
 		return this.keys[__getKey(k)].up;
 	};
 	
-    let IsKeyDown = function (k){
+    InputEngine.prototype.IsKeyDown = function (k){
 		return this.keys[__getKey(k)].down;
 	};
 	
-	let Update = function (deltaTime){
+	InputEngine.prototype.Update = function (deltaTime){
 		for(let i = 0; i < this.keys.Length; i++){
 			this.keys[i].down = false;
 			this.keys[i].up = false;
 		}
 	};
 	
-	function __getKey(c){
+	InputEngine.prototype.__getKey(c){
 		if((typeof c) == 'string') return c.charCodeAt(0);
 		else return c;
 	}
 	
-	function __mouseDown(event){
+	InputEngine.prototype.__mouseDown(event){
 		this.keys[event.button+1].pressed = true;
 		this.keys[event.button+1].down = true;
 	}
 	
-	function __mouseUp(event){
+	InputEngine.prototype.__mouseUp(event){
 		this.keys[event.button+1].pressed = false;
 		this.keys[event.button+1].up = true;
 	}
 	
-    function __mouseMove(event){
-		mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    InputEngine.prototype.__mouseMove(event){
+		this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 	}
 	
 
-	doc.addEventListener( 'mousemove', __mouseMove, false );
-	doc.addEventListener( 'mousedown', __mouseDown, false );
-	doc.addEventListener( 'mouseup', __mouseUp, false );
 
-
-	
-}
