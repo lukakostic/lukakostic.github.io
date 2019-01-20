dd7ad5561f53a4e8dbdad454484a97321587088a
Luka Kostic <3luka1@gmail.com>
2019-01-20 16:23:13 +0100

    Update input.js

--- penguins/penguinEngine/input.js
+++ penguins/penguinEngine/input.js
@@ -6,12 +6,12 @@ class __key{
 	this.pressed = false;
 	this.down = false;
 	this.up = false;
-}
+	}
 }
 
 class InputEngine{
 	
-		IsKeyPressed (k){
+	IsKeyPressed (k){
 		return this.keys[__getKey(k)].pressed;
 	};
 	
@@ -33,22 +33,22 @@ class InputEngine{
 	__getKey (c){
 		if((typeof c) == 'string') return c.charCodeAt(0);
 		else return c;
-	}
+	};
 	
 	__mouseDown(event){
 		this.keys[event.button+1].pressed = true;
 		this.keys[event.button+1].down = true;
-	}
+	};
 	
 	__mouseUp(event){
 		this.keys[event.button+1].pressed = false;
 		this.keys[event.button+1].up = true;
-	}
+	};
 	
     __mouseMove(event){
 		this.mouse.x = ( event.clientX / this.doc.innerWidth ) * 2 - 1;
 		this.mouse.y = - ( event.clientY / this.doc.innerHeight ) * 2 + 1;
-	}
+	};
 	
 	constructor(_doc){
 	
