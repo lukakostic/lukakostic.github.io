97f7409d6c0255ca0f6473d936237f4e121b9df2
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-01-21 20:48:58 +0100

    Update penguins.js

--- penguins/penguins.js
+++ penguins/penguins.js
@@ -21,7 +21,7 @@ plane.position.set(0,-1,0);
 
 			async function init() {
 
-				camera = new THREE.OrthographicCamera(-50,50,50,-50,1,1000);
+				camera = new THREE.OrthographicCamera(-30,30,30,-30,1,1000);
 				//camera = new THREE.PerspectiveCamera( 38, window.innerWidth / window.innerHeight, 1, 1000 );
 				
 				//camera.position.set( 0, 130, -150 );
