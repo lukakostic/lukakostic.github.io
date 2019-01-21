ed1dc3f4d7115d1e2b5c9295edce67682c27de33
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-01-21 21:37:43 +0100

    Update snowballs.js

--- penguins/snowballs.js
+++ penguins/snowballs.js
@@ -6,12 +6,21 @@ function middle(a,b){
 return ((a-b)/2+a);
 }
 
+function findParabola(top,point){
+ return ((point.y-top.y)/((point.x-top.x)**2));
+}
+
+
+function parabolaY(x,a,h,k){
+ return (a*(x-h)**2 + k);
+}
+
 class Snowball{
  constructor(_start,_end){
  this.start = _start;
  this.end = _end;
- this.vertex = new THREE.Vector3(middle(_start.x,_end.x),middle(_start.y,_end.y),middle(_start.z,_end.z));
- this.time = 0;
+ this.vertex = new THREE.Vector3(middle(_start.x,_end.x),middle(_start.y,_end.y)+(_start.distanceTo(_end)/5),middle(_start.z,_end.z));
+ this.a = findParabola(this.vertex,this.start);
  this.object = new THREE.Mesh( new THREE.SphereGeometry( 0.3, 8, 8 ), new THREE.MeshBasicMaterial( {color: 0xffffff} ) );
  this.object.position.set(_start.x,_start.y,_start.z);
   scene.add(this.object);
@@ -26,7 +35,8 @@ function UpdateSnowballs(delta){
 for(let i = 0; i < snowballs.length; i++){
 //snowballs[i].object.position.lerp(snowballs[i].end,delta*13);
  if(snowballs[i].object.position.distanceTo(snowballs[i].end)>0.1){
-snowballs[i].object.position.add((snowballs[i].end.clone().sub(snowballs[i].object.position.position)).normalize().multiplyScalar(delta*13));
+snowballs[i].object.position.add((snowballs[i].end.clone().sub(snowballs[i].object.position)).normalize().multiplyScalar(delta*8));
+  snowballs[i].object.position.setY(parabolaY(snowballs[i].object.position.x,snowballs[i].a,snowballs[i].vertex.x,snowballs[i].vertex.y));
 }
 }
 }
