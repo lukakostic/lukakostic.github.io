88dea6653f5696eef6ad3305349d88c4e74c9d36
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-03-09 03:34:54 +0100

    Create smolmarkup.js

---
+++ smolmarkup/smolmarkup.js
@@ -0,0 +1,18 @@
+class SmolMarkup
+{
+    constructor(str)
+    {
+        this.str = str;
+        this.vars = [];
+        this.compiled = '';
+    }
+
+    compile()
+    {
+    this.compiled = '';
+      for(let i = 0; i < this.str.length; i++)
+      {
+      
+      }
+    }
+}
