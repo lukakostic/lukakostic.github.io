57c658ba27e6ab6748913e3e8cf0a438b0cf25f4
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-03-09 23:20:39 +0100

    Update smolmarkup.js

--- smol/smolmarkup.js
+++ smol/smolmarkup.js
@@ -10,14 +10,16 @@ class SmolMarkup
     compile()
     {
     this.compiled = '';
+        let parent = '';
+        let indentation = 0;
         let word = '';
         let inQuotes = false;
         let gotCommentStart = false;
         let escapeNext = false;
         let inComment = false;
-      for(let i = 0; i < this.str.length; i++)
-      {
-        
-      }
+        for(let i = 0; i < this.str.length; i++)
+        {
+            
+        }
     }
 }
