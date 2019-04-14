630014499e63732414ce8863fa44529ddb078a71
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-04-15 00:24:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ for(let i = 0; i < url.length; i++){
     if(url[i] == '#') filters.unshift('');
     else if(filters.length != 0) filters[0]+=url[i];
 }
-if(filters!=[]){
+if(filters!=[] && filters.every(i=>i=='') == false){
 $("#boards").children().each(function(i) {
     let tags = $(this).data('tags');
     if(filters.some(r=> r != '' && tags.indexOf(r) > -1) == false)
