79c88fe838739e33f07523498b5aa22af1e8a7d8
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-04-15 00:29:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ $("#boards *").filter(function() {
 alert($(this).html());
 });
 */
+
+filterURL();
+
+function filterURL(){
 filters = [];
 
 url = window.location.href;
@@ -12,10 +16,18 @@ for(let i = 0; i < url.length; i++){
     if(url[i] == '#') filters.unshift('');
     else if(filters.length != 0) filters[0]+=url[i];
 }
+
+
+
 if(filters!=[] && filters.every(i=>i=='') == false){
 $("#boards").children().each(function(i) {
     let tags = $(this).data('tags');
     if(filters.some(r=> r != '' && tags.indexOf(r) > -1) == false)
-    $(this).toggle();
+    $(this).hide();
+    else
+    $(this).show();
 });
 }
+}
+
+window.onhashchange = filterURL;
