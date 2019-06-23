40bacc8592f8532366ae8762e2ccb623a38d396d
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-06-23 12:09:43 +0200

    load Text modal

--- lukaboard/board/modalDialog.js
+++ lukaboard/board/modalDialog.js
@@ -1,9 +1,9 @@
 function showTextBoardDialog(){
     var textBtn = event.srcElement;
-    textBtn.getAttribute('data-id');
-    $('#textBoardDialogTitle').val("TITLE");
+    var brd = Board.fromId(textBtn.getAttribute('data-id'));
+    $('#textBoardDialogTitle').val(brd.name);
     let text = $('#textBoardDialogText');
-    text.val("TEXT");
+    text.val(brd.content);
     $('#textBoardDialog').modal('show');
 
     //can do without timeout, but set timeout to like 0.8 seconds if you add 'modal fade' instead of just 'modal'
