4bc9b7fbe5cfc8fb0cc7aeb12ce16a9afb8b4322
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-06-23 12:42:26 +0200

    text dialog

--- lukaboard/board/modalDialog.js
+++ lukaboard/board/modalDialog.js
@@ -1,10 +1,10 @@
 function showTextBoardDialog(){
     var textBtn = event.srcElement;
-    var brd = Board.fromId(textBtn.getAttribute('data-id'));
-
+    var brd = Board.fromId(textBtn.parentNode.getAttribute('data-id'));
 
     $('#textBoardDialogTitle').val(brd.name);
-    $('#textBoardDialogText').val(brd.content);
+    let text = $('#textBoardDialogText');
+    text.val(brd.content);
     let modal = $('#textBoardDialog');
     modal.data('id') = brd.id;
     modal.modal('show');
