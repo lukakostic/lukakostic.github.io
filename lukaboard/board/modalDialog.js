380d6adca1207320a22b1a274fc681cd77400ef6
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-06-23 13:41:15 +0200

    Update modalDialog.js

--- lukaboard/board/modalDialog.js
+++ lukaboard/board/modalDialog.js
@@ -1,14 +1,14 @@
 function showTextBoardDialog(){
     var textBtn = event.srcElement;
     var brd = Board.fromId(textBtn.parentNode.getAttribute('data-id'));
-    
+
     if(brd==null) alert('Text board modal: brd == null');
 
     $('#textBoardDialogTitle').val(brd.name);
     let text = $('#textBoardDialogText');
     text.val(brd.content);
     let modal = $('#textBoardDialog');
-    modal.data('id',brd.id);
+    modal[0].setAttribute('data-id',brd.id);
     modal.modal('show');
 
     //can do without timeout, but set timeout to like 0.8 seconds if you add 'modal fade' instead of just 'modal'
