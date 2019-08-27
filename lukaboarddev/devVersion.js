ff940fe8ced19df59745ac444b50b80e5ada024b
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-08-27 20:46:19 +0200

    pboard

--- lukaboarddev/devVersion.js
+++
@@ -1,8 +0,0 @@
-//let siteUrl = "http://localhost:8000/";
-
-
-let siteUrl = "https://lukakostic.com/lukaboard/";
-
-if(window.location.pathname.includes('lukaboarddev')){
-siteUrl = "https://lukakostic.com/lukaboarddev/";
-}
---
+++ pboarddev/devVersion.js
@@ -0,0 +1,8 @@
+//let siteUrl = "http://localhost:8000/";
+
+
+let siteUrl = "https://lukakostic.com/pboard/";
+
+if(window.location.pathname.includes('pboarddev')){
+siteUrl = "https://lukakostic.com/pboarddev/";
+}
