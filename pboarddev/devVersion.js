adfbe8da29889e2a2366dbb6620ab19ffec29ff3
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2020-04-07 22:23:43 +0200

    Update devVersion.js

--- pboarddev/devVersion.js
+++ pboarddev/devVersion.js
@@ -1,8 +1,8 @@
 //let siteUrl = "http://localhost:8000/";
 
 
-let siteUrl = "https://lukakostic.com/pboard/";
+let siteUrl = "https://lukakostic.github.io/pboard/";
 
 if(window.location.pathname.includes('pboarddev')){
-siteUrl = "https://lukakostic.com/pboarddev/";
+siteUrl = "https://lukakostic.github.io/pboarddev/";
 }
